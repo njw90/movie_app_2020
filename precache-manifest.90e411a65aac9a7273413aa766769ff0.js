af41bb79d191c3b8d44a75c95708f45bd99e9635
njw90 <njw90@pdic.co.kr>
2020-07-25 20:28:59 +0900

    Updates

---
+++ precache-manifest.90e411a65aac9a7273413aa766769ff0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f99e6c9947d3211189f5e10c5b8715d7",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "2acdb57ca1c1c49a0d7c",
+    "url": "/movie_app_2020/static/css/main.15f7c2b5.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2acdb57ca1c1c49a0d7c",
+    "url": "/movie_app_2020/static/js/main.5a3b24fe.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
